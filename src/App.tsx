4171c6f0f3791bb49986a89c613e3a112138514e
aronvaldivia <git@stackblitz.com>
2024-12-05 10:38:08 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">Welcome to PaginaBiix</h1>
+    </div>
+  );
+}
+
+export default App;
